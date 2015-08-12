1027633c20f243106deaa3ca08e88d29ead4fc6f
Teemu Sarapisto <tsarpf@gmail.com>
2015-08-13 02:11:35 +0300

    Webpack serving stuff correctly. Something causes a 'not dom element' error though

--- components/tab-bar.js
+++ components/tab-bar.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import TabBarEntry from './tab-bar-entry';
+import style from '../style/main.css';
 
 export default class TabBar extends React.Component {
     render() {
-        console.log( '-------- tab bar -------' );
-        console.log( <TabBarEntry /> );
-        console.log( '-------- tab bar -------' );
+        let classes = style.left + ' ' + style.col;
         return (
-            <div className='left col'>
+            <div className={classes}>
                 <TabBarEntry />
                 <TabBarEntry />
                 <TabBarEntry />
